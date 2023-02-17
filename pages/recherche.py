e04017bc564b1db8d481c92a6f31deb0923156b1
Nhat-nam <trinhnhatnam75@gmail.com>
2023-02-17 09:05:28 +0100

    "ajout des graphiques"

--- pages/recherche.py
+++ pages/recherche.py
@@ -13,4 +13,19 @@ confirm = st.button("Valider")
 
 if confirm:
     itemId = df[df['nom'].str.contains(itemName)]['identifiant'].values[0]
-    
+    itemResp = requests.get("https://www.albion-online-data.com/api/v2/stats/Charts/{item}.json?locations={location}&time-scale=24".format(item=itemId, location=cityNames[0]))
+    dataItem = itemResp.json()
+    if dataItem == []:
+        st.write("Pas de données pour cet item")
+    else:
+        prices = dataItem[0]['data']['prices_avg']
+        stock = dataItem[0]['data']['item_count']
+        quality = dataItem[0]['quality']
+        location = dataItem[0]['location']
+        dates = pd.to_datetime(dataItem[0]['data']['timestamps'])
+        st.write('Stock sur 1 mois :')
+        st.line_chart(pd.DataFrame({'date': dates, 'stock': stock}), x="date", y="stock")
+        st.write('Prix (en pièces argent) sur 1 mois :')
+        st.line_chart(pd.DataFrame({'date': dates, 'prix': prices}), x="date", y="prix")
+        st.write("La qualité de l'item est : " + str(quality))
+        st.write("Lieu recherché : " + location)
